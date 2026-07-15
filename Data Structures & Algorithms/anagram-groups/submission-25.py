4121964e4c7f75dfd1eddbd04226866e710554c2
ashw8817-dev <ashw8817@gmail.com>
2026-07-15 14:39:52 -0700

    Add: anagram-groups - submission-25

---
+++ Data Structures & Algorithms/anagram-groups/submission-25.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dictionary = {}
+        final_list = []
+        for index, value in enumerate(strs):
+            sorted_item = "".join(sorted(value))
+            if sorted_item in dictionary:
+                dictionary[sorted_item].append(value)
+            else:
+                dictionary[sorted_item] = [value]
+
+        final_list = list(dictionary.values())
+
+        return final_list
+
+                
+
+            
